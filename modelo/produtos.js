054eacbe6df85229352c0cabcfe19ec14bc24cd2
Rodolfo Eliezer S. Rezende <Rodolfo@contabilidadebessa.com.br>
2019-11-28 08:28:08 -0300

    add produto

--- modelo/produtos.js
+++ modelo/produtos.js
@@ -3,13 +3,42 @@ class Produto {
         this.knex = knex;
     }
 
+    async cadastrarProduto(trx, nome, id_praca, preco) {
+        //se não houver nome, não permitir o cadastro.
+        if (!nome) {
+            throw Error('Não é possível cadastrar produto sem um nome');
+        }
+
+        //se não houver preco, não permitir o cadastro
+        if (!preco) {
+            throw Error('Não é possível cadastrar produto sem um preço');
+        }
+
+        //inserir itens no banco de dados;
+        let id_produto = await trx('produto').insert({ nome: nome, id_praca: id_praca }).returning('id_produto');
+        let id_preco = await trx('preco').insert({ id_produto: id_produto, valor: preco, dataCorrente: new Date().toISOString() }).returning('id_preco');
+        return { id_produto, id_preco };
+    }
+
+    /**
+     * @param {string} select 
+     * @param {JSON} where 
+     */
     async obterProdutos(select, where) {
         //const query = this.knex('produto').select().leftJoin('preco', 'preco.id_produto', 'produto.id_produto');
-        where = where ? 'where ' + where : '';
+        let whereClause = 'where ';
+        let condicoes = Object.keys(where);
+        for (let i = 0; i < condicoes.length; i++) {
+            const element = condicoes[i];
+            whereClause += element + "=" + "'" + where[element] + "'";
+            if (i != condicoes.length - 1) {
+                whereClause += ' AND ';
+            }
+        }
         const query = this.knex.raw(`select ${select ? select : '*'} from 
              produto p join preco r on p.id_produto = r.id_produto
              AND dataCorrente = (select max(dataCorrente) from preco where id_produto = p.id_produto) 
-             ${where}
+             ${whereClause}
              order by dataCorrente DESC`)
         return await query;
     }
--- routes/produtoRoute.js
+++ routes/produtoRoute.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer')
+var upload = multer({ dest: 'uploads/' })
 
 router.get('/get_produtos', async function (req, res, next) {
-    const { select, orderby, where } = req.query;
+    const { select, where } = req.query;
     try {
         const produto = req.app.get('produto');
-        const listaProdutos = await produto.obterProdutos(select, orderby, where);
+        const listaProdutos = await produto.obterProdutos(select, where);
         res.send(listaProdutos);
     } catch (err) {
         console.log(err.message);
@@ -13,9 +15,23 @@ router.get('/get_produtos', async function (req, res, next) {
     }
 });
 
-router.post('/post_produto', async function (req, res, next) {
+router.get('/post_produto', async function (req, res, next) {
+    const { nome, id_praca, preco } = req.body;
     try {
-        const { produto, preco } = req.body;
+        let knex = req.app.get('knex');
+        const produto = req.app.get('produto');
+        await knex.transaction(async function (trx) {
+            produto.cadastrarProduto(trx, nome, id_praca, preco);
+        })
+    } catch (err) {
+        console.log(err.message);
+        res.status(500).send(err.message);
+    }
+})
+
+router.post('/post_produto_foto', upload.single('avatar'), async function (req, res, next) {
+    try {
+        const { id_produto } = req.body;
 
     } catch (err) {
         console.log(err.message);
